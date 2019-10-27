97bba17b31c45b62d7e7bae016dd233b5bebb1aa
toxa16 <toxa16@gmail.com>
2019-10-27 20:58:18 +0200

    transmitting machine network status

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const WebSocket = require('ws');
 const ActionType = {
   MACHINE_CONNECT: 'MACHINE_CONNECT',
   CONTROLLER_CONNECT: 'CONTROLLER_CONNECT',
+  MACHINE_OFFLINE: 'MACHINE_OFFLINE',
+  MACHINE_ONLINE: 'MACHINE_ONLINE',
 };
 
 const port = process.env.PORT || 3000;
@@ -10,6 +12,15 @@ const wss = new WebSocket.Server({ port });
 
 const machines = [];
 const controllers = [];
+
+function sendMachineOnline(socket) {
+  const action = { type: ActionType.MACHINE_ONLINE };
+  socket.send(JSON.stringify(action));
+}
+function sendMachineOffline(socket) {
+  const action = { type: ActionType.MACHINE_OFFLINE };
+  socket.send(JSON.stringify(action));
+}
 
 wss.on('connection', function connection(ws, req) {
   const remotePort = req.connection.remotePort;
@@ -19,6 +30,7 @@ wss.on('connection', function connection(ws, req) {
     const action = JSON.parse(message);
     switch (action.type) {
       case ActionType.MACHINE_CONNECT: {
+        // if there is a machine already connected
         if (machines.length > 0) {
           console.log('REJECTED: unable to connect more than one machine.');
           const reaction = {
@@ -28,12 +40,19 @@ wss.on('connection', function connection(ws, req) {
           ws.send(JSON.stringify(reaction));
           ws.close();
         } else {
-          machines.push(remotePort);
+          // pushing new machine's websocket into registry
+          machines.push(ws);
+          // notifying (possible) controller about machine online status
+          const controller = controllers[0];
+          if (controller) {
+            sendMachineOnline(controller);
+          }
           console.log('New machine connected on port', remotePort);
         }
         break;
       }
       case ActionType.CONTROLLER_CONNECT: {
+        // if there is a controller already connected
         if (controllers.length > 0) {
           console.log('REJECTED: unable to connect more than one controller.');
           const reaction = {
@@ -43,7 +62,17 @@ wss.on('connection', function connection(ws, req) {
           ws.send(JSON.stringify(reaction));
           ws.close();
         } else {
-          controllers.push(remotePort);
+          // registering new contoller's websocket
+          controllers.push(ws);
+          // sending machine online status to the controller
+          const machine = machines[0];
+          if (machine) {
+            // if there is a connected machine
+            sendMachineOnline(ws);
+          } else {
+            // if there is no connected machine
+            sendMachineOffline(ws);
+          }
           console.log('New controller connected on port', remotePort);
         }
         break;
@@ -57,10 +86,17 @@ wss.on('connection', function connection(ws, req) {
   });
 
   ws.on('close', (code, reason) => {
-    if (machines.includes(remotePort)) {
+    if (machines.includes(ws)) {
+      // removing machine websocket from registry
       machines.pop();
+      // notifying controller about machine offline status
+      const controller = controllers[0];
+      if (controller) {
+        sendMachineOffline(controller);
+      }
       console.log(`Machine ${remotePort} disconnected with code ${code}`);
-    } else if (controllers.includes(remotePort)) {
+    } else if (controllers.includes(ws)) {
+      // removing controller socket from registry
       controllers.pop();
       console.log(`Controller ${remotePort} disconnected with code ${code}`);
     } else{
@@ -68,14 +104,14 @@ wss.on('connection', function connection(ws, req) {
     }
   });
 
-  const getRandomStatus = () => {
+  /*const getRandomStatus = () => {
     if (Math.random() >= 0.5) {
       return 'MACHINE_ONLINE';
     }
     return 'MACHINE_OFFLINE';
   }
   const action = { type: getRandomStatus() };
-  ws.send(JSON.stringify(action));
+  ws.send(JSON.stringify(action));*/
 });
 
 wss.on('listening', () => {
