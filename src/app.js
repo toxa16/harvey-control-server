b5cfa7a3b0a5bb3ab5d5b08615fae43321113d1e
toxa16 <toxa16@gmail.com>
2019-10-27 22:49:34 +0200

    refactoring index script

---
+++ src/app.js
@@ -0,0 +1,108 @@
+const ActionType = require('./action-type.enum');
+
+const machines = [];
+const controllers = [];
+
+function sendMachineOnline(socket) {
+  const action = { type: ActionType.MACHINE_ONLINE };
+  socket.send(JSON.stringify(action));
+}
+function sendMachineOffline(socket) {
+  const action = { type: ActionType.MACHINE_OFFLINE };
+  socket.send(JSON.stringify(action));
+}
+
+function app(ws, req) {
+  const remotePort = req.connection.remotePort;
+  console.log('client connected', remotePort);
+
+  ws.on('message', function incoming(message) {
+    const action = JSON.parse(message);
+    switch (action.type) {
+      case ActionType.MACHINE_CONNECT: {
+        // if there is a machine already connected
+        if (machines.length > 0) {
+          console.log('REJECTED: unable to connect more than one machine.');
+          const reaction = {
+            type: ActionType.MACHINE_REJECTED,
+            error: 'Unable to connect more than one machine.',
+          };
+          ws.send(JSON.stringify(reaction));
+          ws.close();
+        } else {
+          // pushing new machine's websocket into registry
+          machines.push(ws);
+          // notifying the machine that it has been accepted by the server
+          const reaction = {
+            type: ActionType.MACHINE_ACCEPTED,
+          };
+          ws.send(JSON.stringify(reaction));
+          // notifying (possible) controller about machine online status
+          const controller = controllers[0];
+          if (controller) {
+            sendMachineOnline(controller);
+          }
+          console.log('New machine connected on port', remotePort);
+        }
+        break;
+      }
+      case ActionType.CONTROLLER_CONNECT: {
+        // if there is a controller already connected
+        if (controllers.length > 0) {
+          console.log('REJECTED: unable to connect more than one controller.');
+          const reaction = {
+            type: ActionType.CONTROLLER_REJECTED,
+            error: 'Unable to connect more than one controller.',
+          };
+          ws.send(JSON.stringify(reaction));
+          ws.close();
+        } else {
+          // registering new contoller's websocket
+          controllers.push(ws);
+          // notifying the controller that it has been accepted by the server
+          const reaction = {
+            type: ActionType.CONTROLLER_ACCEPTED,
+          };
+          ws.send(JSON.stringify(reaction));
+          // sending machine online status to the controller
+          const machine = machines[0];
+          if (machine) {
+            // if there is a connected machine
+            sendMachineOnline(ws);
+          } else {
+            // if there is no connected machine
+            sendMachineOffline(ws);
+          }
+          console.log('New controller connected on port', remotePort);
+        }
+        break;
+      }
+      default: {
+        console.log('Unknown action:', action);
+        const reaction = { type: 'UNKNOWN_ACTION' };
+        ws.send(JSON.stringify(reaction));
+      }
+    }
+  });
+
+  ws.on('close', (code, reason) => {
+    if (machines.includes(ws)) {
+      // removing machine websocket from registry
+      machines.pop();
+      // notifying controller about machine offline status
+      const controller = controllers[0];
+      if (controller) {
+        sendMachineOffline(controller);
+      }
+      console.log(`Machine ${remotePort} disconnected with code ${code}`);
+    } else if (controllers.includes(ws)) {
+      // removing controller socket from registry
+      controllers.pop();
+      console.log(`Controller ${remotePort} disconnected with code ${code}`);
+    } else{
+      console.log(`Unknown client ${remotePort} disconnected with code ${code}`);
+    }
+  });
+}
+
+module.exports = app;
